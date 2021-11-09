ad30abac938c12ddb8ef983c853e08973799eb0e
khalil <khalil94bouzayeni^gmail.com>
2021-11-09 11:54:04 +0100

    react js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button,Form,Navbar,Container,Nav} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+
+    <Navbar.Brand href="#home" className="main">Home</Navbar.Brand>
+    
+      <Nav.Link href="#home"className="main">About</Nav.Link>
+      <Nav.Link href="#features"className="main">Features</Nav.Link>
+      <Nav.Link href="#pricing"className="main">Pricing</Nav.Link>
+    
+    </Container>
+  </Navbar>
+  
+  
+</>
+       <br/>
+        <Button> Hey Try me </Button>
+        <br/>
+        <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary"className="done" type="submit">
+    Submit
+  </Button>
+</Form>
+
+        
       </header>
     </div>
   );
